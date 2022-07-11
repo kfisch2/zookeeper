26bdd2e815456c7ac5282330a0581b5ab7e28ab6
kfisch2 <kmlindsey24@gmail.com>
2022-07-11 16:25:45 -0700

    fixed port error?

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { animals } = require('./data/animals.json')
 const express = require('express');
 const { query } = require('express');
 
+const PORT = process.env.PORT || 3001; 
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -55,8 +57,8 @@ app.get('/api/animals', (req, res) => {
 
 
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`)
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
 
